cd32be8e2968985ad42faa802070154a22e7774b
Stas015 <167779562+Stas015@users.noreply.github.com>
2024-04-22 15:32:41 +0200

    Add files via upload

---
+++ hw2.js
@@ -0,0 +1,11 @@
+// Oголошення та ініціалізація змінної константи (const)
+const batterConsistency = "thick homogeneous";
+const patelniaTemperature = "220";
+const cookingTime = "30";
+const flipCount = "2";
+const ingredientList =  ["milk", "oil", "flour","eggs"];
+const batterVolume = "1/3";
+const servingSize = "30";
+const toppingChoice = ["raspberry"];
+const remainingBatter = "true";
+const patelniaOilLevel = "10%";
